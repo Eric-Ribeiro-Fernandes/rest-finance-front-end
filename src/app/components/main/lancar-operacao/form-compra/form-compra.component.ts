5d2c758cd16f19cf3e1c7e2870c5635aedd09301
F2964862 Eric Ribeiro Fernandes <eric.ribeiro@bb.com.br>
2023-11-22 23:16:18 -0300

    refinando validações do formulario de compra

--- src/app/components/main/lancar-operacao/form-compra/form-compra.component.ts
+++ src/app/components/main/lancar-operacao/form-compra/form-compra.component.ts
@@ -4,6 +4,7 @@ import {
   FormGroup,
   FormControl,
   ReactiveFormsModule,
+  Validators,
 } from '@angular/forms';
 
 @Component({
@@ -14,15 +15,46 @@ import {
 export class FormCompraComponent implements OnInit {
   constructor(private formBuilder: FormBuilder) {}
 
+  data = '';
+  ticker = '';
+  precoPago = 0;
+  quantidade = 1;
+  totalCompra = 0;
   form!: FormGroup;
 
   ngOnInit(): void {
+    // Criação do formulário
     this.form = this.formBuilder.group({
-      data: [''],
-      ticker: [''],
-      precoPago: [''],
-      quantidade: [''],
-      totalCompra: [''],
+      data: [this.data, Validators.compose([Validators.required])],
+      ticker: [
+        this.ticker,
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(5),
+          ,
+          Validators.maxLength(6),
+        ]),
+      ],
+      precoPago: [this.precoPago, Validators.required],
+      quantidade: [this.quantidade, Validators.required],
+      totalCompra: [this.totalCompra],
+    });
+
+    // Campos reativos
+    this.form.controls['precoPago'].valueChanges.subscribe((valor) => {
+      const qtd = this.form.get('quantidade')?.value;
+
+      this.totalCompra = valor * qtd;
+
+      this.form.controls['totalCompra'].setValue(this.totalCompra);
+    });
+
+    this.form.controls['quantidade'].valueChanges.subscribe((valor) => {
+      const preco = this.form.get('precoPago')?.value;
+
+      this.totalCompra = valor * preco;
+
+      this.form.controls['totalCompra'].setValue(this.totalCompra);
     });
   }
 }
